c64bbe250c87c485f515856ac5b4b838f9439a58
Luis Roberto Weck <luisrweck@gmail.com>
2026-09-12 01:12:22 -0300

    katex-render: re-troca helper para funcionar com katex lazy (MutationObserver)

---
+++ assets/js/katex-render.js
@@ -0,0 +1,27 @@
+const renderMath = () => {
+  if (window.renderMathInElement) {
+    renderMathInElement(document.body);
+  }
+};
+
+const hookKatexLoader = (scriptElement) => {
+  if (window.katex) {
+    renderMath();
+  } else {
+    scriptElement.addEventListener("load", renderMath);
+  }
+};
+
+let katexLoader = document.getElementById("katex-render");
+if (katexLoader) {
+  hookKatexLoader(katexLoader);
+} else if ("MutationObserver" in window) {
+  const observer = new MutationObserver(() => {
+    katexLoader = document.getElementById("katex-render");
+    if (katexLoader) {
+      observer.disconnect();
+      hookKatexLoader(katexLoader);
+    }
+  });
+  observer.observe(document.documentElement, { childList: true, subtree: true });
+}
