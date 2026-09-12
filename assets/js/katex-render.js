fe41b715733012c32786f0ba4cbfba9075ca0376
Luis Roberto Weck <luisrweck@gmail.com>
2026-09-12 01:30:31 -0300

    katex: ativa math sem shortcode explicito (detecta $..$/$$) e suporta inline $$$, $...$

--- assets/js/katex-render.js
+++ assets/js/katex-render.js
@@ -1,6 +1,13 @@
 const renderMath = () => {
   if (window.renderMathInElement) {
-    renderMathInElement(document.body);
+    renderMathInElement(document.body, {
+      delimiters: [
+        { left: "$$", right: "$$", display: true },
+        { left: "$", right: "$", display: false },
+        { left: "\\(", right: "\\)", display: false },
+        { left: "\\[", right: "\\]", display: true },
+      ],
+    });
   }
 };
 
